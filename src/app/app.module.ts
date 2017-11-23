7abec249628435b6c233130c80890ba64e8258ee
topankajpandey <topankajpandey@gmail.com>
2017-11-23 18:26:45 +0530

    Design modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-<<<<<<< HEAD
-import { LoginPage } from '../pages/login/login';
-=======
 import { TabsPage } from '../pages/tabs/tabs';
 
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
 import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 import { BiblePage } from '../pages/bible/bible';
@@ -15,13 +11,8 @@ import { SermonsPage } from '../pages/sermons/sermons';
 import { UsersPage } from '../pages/users/users';
 import { PrayerPage } from '../pages/prayer/prayer';
 import { ContactPage } from '../pages/contact/contact';
-<<<<<<< HEAD
 import { ChatPage } from '../pages/chat/chat';
 import { ChattingPage } from '../pages/chatting/chatting';
-=======
-
-
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -29,10 +20,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-<<<<<<< HEAD
-    LoginPage,
-=======
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
     HomePage,
     AboutPage,
     BiblePage,
@@ -40,13 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     UsersPage,
     PrayerPage,
     ContactPage,
-<<<<<<< HEAD
 	ChatPage,
-	ChattingPage
-=======
-
+	ChattingPage,
     TabsPage
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
   ],
   imports: [
     BrowserModule,
@@ -55,24 +38,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-<<<<<<< HEAD
-    LoginPage,
-=======
     TabsPage,
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
     HomePage,
     AboutPage,
     BiblePage,
     SermonsPage,
     UsersPage,
     PrayerPage,
-<<<<<<< HEAD
     ContactPage,
-	  ChattingPage,
-  	ChatPage
-=======
-    ContactPage
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
+	ChattingPage,
+	ChatPage
 
   ],
   providers: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-<<<<<<< HEAD
 import { ChatPage } from '../../pages/chat/chat';
-=======
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
 
 @Component({
   selector: 'page-home',
@@ -12,17 +9,10 @@ import { ChatPage } from '../../pages/chat/chat';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-<<<<<<< HEAD
-    
+
   }
 
 	chat(){
 	 this.navCtrl.setRoot(ChatPage);
-  }
+	 }
 }
-=======
-
-  }
-
-}
->>>>>>> 24c968bfa91b64575719c1a9e8653ee76db217e1
